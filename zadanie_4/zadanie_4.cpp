e9a1329964bf8c871a7e1a5474b47712295ba07e
Bartek <bartek9393@poczta.onet.pl>
2017-07-02 21:04:34 +0200

    debug

--- zadanie_4/zadanie_4.cpp
+++ zadanie_4/zadanie_4.cpp
@@ -3,74 +3,81 @@
 #include <vector>
 
 
-bool isAlreadyInArray(int number);
-
-int getSum(int n);
-
-std::vector<int> vector;
-
 int main() {
+    std::vector<int> vector;
+
     int n;
     std::cin >> n;
-    if (n < 1) return 0;
+    if (n < 1) {
+        return 0;
+    }
     int array[n][n];
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             int number;
             std::cin >> number;
-            if (number < 1) return 0;
-            if (isAlreadyInArray(number)) {
-                std::cout << -1;
+            if (number < 1) {
                 return 0;
+            }
+
+            for (int k = 0; k < vector.size(); ++k) {
+                if (vector[k] == number) {
+                    std::cout << -1;
+                    return 0;
+                }
+                if (vector[k] > n * n) {
+                    std::cout << -1;
+                    return 0;
+                }
             }
             vector.push_back(number);
             array[j][i] = number;
         }
     }
 
-    int sum = getSum(n);
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += array[i][0];
+    }
 
     for (int i = 0; i < n; ++i) {
         int countedSum = 0;
         for (int j = 0; j < n; ++j) {
+            std::cout << "[" << j << "," << i << "] " << array[j][i];
             countedSum += array[j][i];
         }
         if (sum != countedSum) {
             std::cout << -1;
             return 0;
         }
+        std::cout << std::endl;
     }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
 
     for (int i = 0; i < n; ++i) {
         int countedSum = 0;
         for (int j = 0; j < n; ++j) {
+            std::cout << "[" << j << "," << i << "] " << array[i][j];
             countedSum += array[i][j];
         }
         if (sum != countedSum) {
             std::cout << -1;
             return 0;
         }
+        std::cout << std::endl;
     }
 
-    std::cout << sum << std::endl;
+    std::cout << std::endl << sum << std::endl;
 
     return 0;
 }
 
-bool isAlreadyInArray(int number) {
-    for (int i = 0; i < vector.size(); ++i) {
-        if (vector[i] == number) {
-            return true;
-        }
-    }
-    return false;
-}
-
-int getSum(int n) {
-    int sum = 0;
-    for (int i = 0; i < n; ++i) {
-        sum += vector[i];
-    }
-    return sum;
-}
+//std::cout << "[" << j << "," << i << "] " << number;        std::cout << std::endl;
